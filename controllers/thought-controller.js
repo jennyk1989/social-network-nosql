cc0a6ce7c255bcac4ee7ae3d2f26380136ec6ec4
Jenny Keene <jennifer0keene@gmail.com>
2022-02-26 15:19:24 -0600

    thought controller skeleton

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,11 +1,37 @@
 // import Thought, User models
-// routes to /api/thoughts
-// GET to get all thoughts
-// GET to get single thought by _id
-// POST to create new thought (push created thought's _id to associated user's thoughts array field)
-// PUT to update thought by _id
-// DELETE to remove thought by _id
+const { Thought, User } = require('../models');
 
-// routes to /api/thoughts/:thoughtId/reactions:
-// POST to create reaction stored in a single thought's reactions array field
-// DELETE to pull & remove a reaction by the reaction's reactionId value
+// routes to /api/thoughts
+const thoughtControlller = {
+    // GET to get all thoughts
+    getAllThoughts() {
+
+    },
+    // GET to get single thought by _id
+    getThoughtById() {
+
+    },
+    // POST to create new thought (push created thought's _id to associated user's thoughts array field)
+    createThought() {
+
+    },
+    // PUT to update thought by _id
+    updateThought() {
+
+    },
+    // DELETE to remove thought by _id
+    deleteThought() {
+
+    },
+    // routes to /api/thoughts/:thoughtId/reactions:
+    // POST to create reaction stored in a single thought's reactions array field
+    createReaction() {
+
+    },
+    // DELETE to pull & remove a reaction by the reaction's reactionId value
+    deleteReaction() {
+
+    },
+};
+
+module.exports = thoughtControlller;
