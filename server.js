bab3adeb59cd0d2866835a6d1198292a3ce702ba
Jenny Keene <jennifer0keene@gmail.com>
2022-02-25 23:44:49 -0600

    Update server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-nosql', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//log MongoDB queries that are executed:
+mongoose.set('debug',true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`))
