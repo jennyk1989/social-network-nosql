e20463fac5a73281120fbabfa17f32024f598b90
Jenny Keene <jennifer0keene@gmail.com>
2022-02-26 02:35:00 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,44 @@
-//username: string, unique, required, trimemed
+//import mongoose's Schema constuctor and model fx
+const { Schema, model } = require('mongoose')
+//date formatting import
 
-//email: string, required, unique, validation
+// create user schema
+const UserSchema = new Schema(
+    {
+        //username field: string, unique, required, trimmed
+        username: {
+            type: String,
 
-//thoughts: array of _id vals referencing Thought model
+        },
+        //email: string, required, unique, validation
+        email: {
 
-//friends: array of _id vals referencing User model (self-reference)
+        },
+        //thoughts: array of _id vals referencing Thought model
+        thoughts: {
+
+        },
+        //friends: array of _id vals referencing User model (self-reference)
+        friends: {
+
+        }
+    },
+    {
+        toJSON: {
+            //tell schema that it can use virtuals 
+            virtuals: true,
+            //getters here if needed
+        },
+        id: false //prevents virtual's duplication of _id as 'id'
+    }
+);
 
 //Schema: Create virtual friendCount that retrieves length of user's friends array field on query
+UserSchema.virtual('friendCount').get( function() {
+    //length of array
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema); //creating User model
+//export the User model:
+module.exports = User;
