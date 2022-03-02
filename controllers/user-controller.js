0a073997798322b16b07e82654ed68e0df28e557
Jenny Keene <jennifer0keene@gmail.com>
2022-03-02 14:10:18 -0600

    Update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,18 +2,19 @@ const { User } = require('../models'); //import the User model
 
 //routes to /api/users...
 const userController = {
-    
     // get all users
-    getAllUsers() {
-        User.find({}) //mongoose's find() method
+    getAllUsers(req,res) {
+        User.find({}) 
             .then(userdata => res.json(userdata))
             .catch(err => res.status(400).json(err));
     },
     // get single user by _id & populated thought & friend data
     getUserById({ params }, res) { //destructuring params out of req as only need params
         User.findOne({ _id: params.id })
+            //populate thought and friend data:
             .populate('thoughts')
             .populate('friends')
+            //use select option + minus to remove __v from results:
             .select('-__v')
             .then(userdata => {
                 if(!userdata) {
@@ -31,10 +32,11 @@ const userController = {
             .catch(err => res.status(400).json(err))
     },
     // put to update a user by _id
-    updateUser({ params, body }, res) {
+    updateUser({ params, body }, res) { //desctructuring params & body out of req
         User.findOneAndUpdate(
+            //update by _id:
             { _id: params.id }, 
-            body, 
+            body, //user info in body
             { new: true, runValidators: true } //want updated (new) User returned and want it validated
         )
             .then(userdata => {
@@ -48,7 +50,7 @@ const userController = {
     },
     // delete to remove user by _id
     deleteUser({ params }, res) {
-        User.findOneAndDelete({ _id: params.id })
+        User.findOneAndDelete({ _id: params.id }) //delete by _id (get id out of params object)
             .then(userdata => {
                 if(!userdata) {
                     res.status(404).json({ message: 'User not found'});
@@ -59,18 +61,15 @@ const userController = {
             .catch(err => res.status(400).json(err));
     },
     //routes to /api/users/:userId/friends/:friendId...
-    // POST to add new freind 
+    // POST to add new friend 
     addFriend({ params }, res) {
         User.findOneAndUpdate(
+            //get the user by id (user that is adding a friend)
             { _id: params.id },
-            { $push: { friends: params.friendId }}, //MongoDB's push operator to add friend to friends array
-            { new: true, runValidators: true }
+            //use $addToSet instead of $push so friend is not added to friends array if already present
+            { $addToSet: { friends: params.friendId }}, 
+            { runValidators: true }
         )
-        .populate({
-            path: 'friends',
-            select: ('-__V')
-        })
-        .select('-__v')
         .then(userdata => {
             if(!userdata) {
                 res.status(404).json({ message: 'User not found'});
@@ -83,15 +82,12 @@ const userController = {
     // DELETE to remove friend 
     deleteFriend({ params }, res) {
         User.findOneAndUpdate(
+            //get the user by id (user that is deleting a friend)
             { _id: params.id },
-            { $pull: { friends: params.friendId }}, //MongoDB's pull operator to remove friend from friends array
-            { new: true }
+            //MongoDB's pull operator to remove friend from friends array
+            { $pull: { friends: params.friendId }}, //identify friend to remove by friendId
+            { runValidators: true }
         )
-        .populate({
-            path: 'friends',
-            select: ('-__V')
-        })
-        .select('-__v')
         .then(userdata => {
             if(!userdata) {
                 res.status(404).json({ message: 'User not found'});
@@ -103,4 +99,5 @@ const userController = {
     },
 };
 
+//export the controller 
 module.exports = userController;
