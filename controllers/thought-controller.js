49b2d85cec6b35ad3a9476bc11e24ea59be05909
Jenny Keene <jennifer0keene@gmail.com>
2022-03-12 12:06:04 -0600

    Update thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,5 @@
 // import Thought, User models
-const { Thought } = require('../models/Thought');
-const User = require('../models/User');
+const { Thought, User } = require('../models');
 
 // routes to /api/thoughts
 const thoughtController = {
@@ -12,8 +11,10 @@ const thoughtController = {
     },
     // GET to get single thought by _id
     getThoughtById({ params }, res) { //destructured params out of req object
-        Thought.findOne({ _id: params.thoughtId })//find the thought by its _id
+        Thought.findOne({ _id: params.id })//find the thought by its _id
+            .select('-__v') //get rid of __v from json response
             .then(thoughtData => {
+                console.log(thoughtData);
                 if(!thoughtData) {
                     res.status(404).json({ message: 'User data not found'});
                     return;
@@ -44,10 +45,10 @@ const thoughtController = {
             .catch(err => res.status(400).json(err));
     },
     // PUT to update thought by _id
-    updateThought({ params, body}, res) {
+    updateThought({ params, body }, res) {
         Thought.findOneAndUpdate(
-            // find the thought to be updated by id (thoughtId)
-            { _id: params.thoughtId }, 
+            // find the thought to be updated by id
+            { _id: params.id }, 
             body, // body contains all thought info
             { new: true, runValidators: true}
         )
@@ -62,20 +63,7 @@ const thoughtController = {
     },
     // DELETE to remove thought by _id
     deleteThought({ params }, res) {
-        Thought.findOneAndDelete({ _id: params.thoughtId }) //find thought to be deleted by thoughtId
-            .then(deleteData => {
-                if(!deleteData) {
-                    res.status(404).json({ message: 'User data not found'});
-                    return;
-                }
-                return User.findOneAndUpdate(
-                    // find User that thought belongs to
-                    { _id: params.userId },
-                    // remove the thought from the thoughts array
-                    { $pull: { thoughts: params.thoughtId }},
-                    { new: true }
-                );
-            })
+        Thought.findOneAndDelete({ _id: params.id }) //find thought to be deleted by id
             .then(thoughtData => {
                 if(!thoughtData) {
                     res.status(404).json({ message: 'User data not found'});
@@ -92,8 +80,8 @@ const thoughtController = {
             //find thought reaction belong to
             { _id: params.thoughtId },
             // add the reaction (contained in body) to the reactions array 
-            { $addToSet: { reactions: body } },
-            { new: true, runValidators: true }
+            { $push: { reactions: body } },
+            { new: true }
         )
             .then(thoughtData => {
                 if(!thoughtData) {
@@ -111,7 +99,7 @@ const thoughtController = {
             { _id: params.thoughtId },
             //remove reaction (identified by reactionId) from reactions array
             { $pull: { reactions: { reactionId: params.reactionId } } },
-            { runValidators: true, new: true }
+            { new: true }
         )
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err));
